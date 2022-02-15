0949b06f27ec422c2481590cb09c82fe65bce835
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2022-02-15 23:05:39 +0200

    add

---
+++ excercise_1/exc-1-1.js
@@ -0,0 +1,6 @@
+var input = [1, 3, 6, 0, 5, -2, 9, 11, 16];
+
+//TODO
+
+
+console.log(total) ; // 20
---
+++ excercise_1/exc-1-3.js
@@ -0,0 +1,7 @@
+var input4 = [1, 2, 2, 4, 6, 9, 11, 12, 11, 666];
+
+function findModus(arr) {
+    //TODO
+}
+
+console.log(findModus(input4)); //[ 2, 11 ]
