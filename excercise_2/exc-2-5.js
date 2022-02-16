0c1f29204f432f8d9e456387390f456378ba1f67
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2022-02-16 22:08:27 +0200

    add

--- excercise_2/exc-2-5.js
+++ excercise_2/exc-2-5.js
@@ -1,7 +1,18 @@
-var changes = "+++-+---++--+-+-++";
+var changes = '+++-+---++--+-+-++';
 
 function countOfChages(str) {
-    //TODO
+  let count = 0;
+
+  let specialSymbol = '+';
+  const splitStr = str.split('');
+
+  splitStr.forEach(element => {
+    if (element === specialSymbol) {
+      count++;
+    }
+  });
+
+  return count;
 }
 
 console.log(countOfChages(changes)); //10
