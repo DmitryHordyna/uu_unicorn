12b16d65285d02ff24d4510e89e3f29e7e219d86
DmitryHordyna <71545910+DmitryHordyna@users.noreply.github.com>
2022-02-16 23:11:24 +0200

    addd

--- hello-world.js
+++ hello-world.js
@@ -14,46 +14,46 @@
 //     id: 15
 //   }
 // ];
-let users = [
-  {
-    name: 'Peter',
-    age: 32
-  }
-  //   {
-  //     name: 'John',
-  //     age: 15,
-  //     random: 12345
-  //   },
-  //   {
-  //     name: 'Jim',
-  //     age: 16,
-  //     id: 15
-  //   },
-  //   {
-  //     nul: null,
-  //     undef: undefined
-  //   }
-];
-let length = users.length;
-for (let i = 0; i < length; i += 1) {
-  addGettersAndSetters(users[i]);
-}
+// let users = [
+//   {
+//     name: 'Peter',
+//     age: 32
+//   }
+//   {
+//     name: 'John',
+//     age: 15,
+//     random: 12345
+//   },
+//   {
+//     name: 'Jim',
+//     age: 16,
+//     id: 15
+//   },
+//   {
+//     nul: null,
+//     undef: undefined
+//   }
+// ];
+// let length = users.length;
+// for (let i = 0; i < length; i += 1) {
+//   addGettersAndSetters(users[i]);
+// }
 
-function addGettersAndSetters(obj) {
-  for (key in obj) {
-    obj['get' + key.split('')[0].toUpperCase() + key.slice(1)] = function () {
-      return self[key];
-    };
-    obj['set' + key.split('')[0].toUpperCase() + key.slice(1)] = function (
-      newValue
-    ) {
-      self[key] = newValue;
-      return newValue;
-    };
-  }
-  return obj;
-}
+// function addGettersAndSetters(obj) {
+//   for (key in obj) {
+//     obj['get' + key.split('')[0].toUpperCase() + key.slice(1)] = function () {
+//       return self[key];
+//     };
+//     obj['set' + key.split('')[0].toUpperCase() + key.slice(1)] = function (
+//       newValue
+//     ) {
+//       self[key] = newValue;
+//       return newValue;
+//     };
+//   }
+//   return obj;
+// }
 
-console.log(users[0]);
-console.log(users[0].getName());
-console.log(users[0].getAge);
+// console.log(users[0]);
+// console.log(users[0].getName());
+// console.log(users[0].getAge);
